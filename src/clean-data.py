9e6b21047f4b95fc86b7323c31cf73a592ce73c4
Luis Sarmiento <148011762+P4rz1val22@users.noreply.github.com>
2024-07-18 21:03:52 -0400

    Luis (#20)
    * algo fixes :)
    
    * erm...
    
    * CSV file to s3 bucket
    
    - Can now upload the clean data file to the s3 bucket.
    - If the file is already present in the bucket, the user is prompted again.
    
    ---------
    
    Co-authored-by: owensimpson <owensimpson1127@gmail.com>

--- src/clean-data.py
+++ src/clean-data.py
@@ -1,7 +1,11 @@
+import boto3
 import pandas as pd
+import numpy as np
+from botocore.exceptions import NoCredentialsError
 from dotenv import load_dotenv
 import re
 from datetime import datetime
+from botocore.exceptions import NoCredentialsError, ClientError
 
 # Load the .env file
 load_dotenv()
@@ -17,22 +21,21 @@ df.drop(
         "Status",
         "Current Owners",
         "Current Owners Address",
-        # "PDS Property ID",
+        "PDS Property ID",
         "PDS Listing ID",
         "Government Number",
         "Parent Government Number",
+        "Building Area",
+        "Street Display",
     ],
     inplace=True,
 )
 
 # Drop rows where Property Type is not Housing or Unit
-df = df[df["Property Type"].isin(["Housing", "Unit"])]
+df = df[df["Property Type"].isin(["House", "Unit"])]
 
 # Drop rows where any area value is 0
 df = df[~(df[["Area"]] == 0).any(axis=1)]
-
-# Drop rows where Street Display contains a hyphen or slash
-# df = df[~df["Street Display"].fillna("").str.contains(r"[-/]", na=False)]
 
 # Regular expression pattern to find weekly price
 pattern = r"\$?(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)\s*(?!(?:per\s*month|month|/m))\s*(?:per\s*week|week|wk|w|pw)?"
@@ -46,8 +49,6 @@ def extract_weekly_price(text):
         price_str = price_str.replace(",", "")
         price = int(float(price_str))  # Convert to float and then to integer
         return price
-    else:
-        print(text)
     return None
 
 
@@ -65,7 +66,34 @@ def get_relative_month(text):
 df["Weekly Price"] = df["Listed Price"].apply(lambda x: extract_weekly_price(str(x)))
 df.drop(columns=["Listed Price"], inplace=True)
 
-df = df.dropna()
+# Compute the average number of bathrooms per bedroom, ignoring rows where bedrooms or bathrooms are zero
+bathrooms_per_bedroom = (
+        df.loc[(df["Bedrooms"] != 0) & (df["Bathrooms"] != 0), "Bathrooms"]
+        / df.loc[(df["Bedrooms"] != 0) & (df["Bathrooms"] != 0), "Bedrooms"]
+)
+avg_bathrooms_per_bedroom = bathrooms_per_bedroom.mean()
+
+# Update the "Bathrooms" column where it is 0 and "Bedrooms" is not
+df.loc[(df["Bathrooms"] == 0) & (df["Bedrooms"] != 0), "Bathrooms"] = np.round(
+    df["Bedrooms"] * avg_bathrooms_per_bedroom
+)
+
+# Compute the average number of bedrooms per bathroom, ignoring rows where bedrooms or bathrooms are zero
+bedrooms_per_bathroom = (
+        df.loc[(df["Bedrooms"] != 0) & (df["Bathrooms"] != 0), "Bedrooms"]
+        / df.loc[(df["Bedrooms"] != 0) & (df["Bathrooms"] != 0), "Bathrooms"]
+)
+avg_bedrooms_per_bathroom = bedrooms_per_bathroom.mean()
+
+# Update the "Bedrooms" column where it is 0 and "Bathrooms" is not
+df.loc[(df["Bedrooms"] == 0) & (df["Bathrooms"] != 0), "Bedrooms"] = np.round(
+    df["Bathrooms"] * avg_bedrooms_per_bathroom
+)
+
+print("Average bathrooms per bedroom:", avg_bathrooms_per_bedroom)
+print("Average bedrooms per bathroom:", avg_bedrooms_per_bathroom)
+
+df = df.dropna(subset=["Month Listed", "Postcode"])
 
 # Fix month columns
 df["Relative Month"] = df["Month Listed"].apply(lambda x: get_relative_month(str(x)))
@@ -77,9 +105,75 @@ df["Locality"] = df["Locality"].str.upper()  # Convert locality to uppercase
 
 df["Street Name"] = df["Street Name"].str.upper()  # Convert street_name to uppercase
 df["Locality"] = df["Locality"].str.upper()  # Convert locality to uppercase
-df["Postcode"] = df["Postcode"].astype(str)  # Convert postcode to string if not already
+df["Postcode"] = df["Postcode"].apply(
+    lambda x: "X" + str(int(x))
+)  # Convert postcode to string if not already
 
 print(len(df))
 
+df.to_csv("../data/cleaned_housing_data-nulls-fixed.csv", index=False)
 
-df.to_csv("../data/cleaned_housing_data.csv", index=False)
+
+def upload_to_s3(file_name, bucket, object_name=None):
+    """
+    Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Creating an S3 client
+    s3_client = boto3.client("s3")
+
+    # Check if the object already exists in the bucket
+    try:
+        s3_client.head_object(Bucket=bucket, Key=object_name)
+        print(
+            f"Error: The file '{object_name}' already exists in the bucket '{bucket}'.\n"
+        )
+        object_name = (
+                input("Enter the name of the file OR type 'cancel' to cancel the upload: ")
+                + ".csv"
+        )
+        upload_to_s3(file_name, bucket, object_name)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            # The object does not exist, can proceed with the upload
+            pass
+        else:
+            # Something else has gone wrong
+            print(f"Error checking if object exists: {e}")
+            return False
+
+    # Upload the file
+    s3_client = boto3.client("s3")
+    try:
+        s3_client.upload_file(file_name, bucket, object_name)
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+    return True
+
+
+# Example usage
+file_name = "../data/cleaned_housing_data-nulls-fixed.csv"
+bucket_name = "team-houses-bucket"
+object_name = (
+        input("Enter the name of the file OR type 'cancel' to cancel the upload: ") + ".csv"
+)
+if object_name.casefold() != "cancel.csv".casefold():
+    success = upload_to_s3(file_name, bucket_name, object_name)
+    if success:
+        print("Upload Successful")
+    else:
+        print("Upload Failed")
+else:
+    print("Upload Cancelled")
